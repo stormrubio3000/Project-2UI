814678975697432405bec48cf5a5cc72d16871dc
Daniel Rendon <danielrendon93@gmail.com>
2019-03-22 14:47:03 -0500

    Auth v1

--- ANightsTaleUI/ANightsTaleUI/Startup.cs
+++ ANightsTaleUI/ANightsTaleUI/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,8 +32,9 @@ namespace ANightsTaleUI
 				options.MinimumSameSitePolicy = SameSiteMode.None;
 			});
 
+            services.AddSingleton<HttpClient>();
 
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
