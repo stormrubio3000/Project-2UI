e8c75496991b517bcef907efae068c9e68849e34
stormrubio3000 <storm-3000@hotmail.com>
2019-03-22 10:17:42 -0500

    Added the Framework for Campaign

---
+++ ANightsTaleUI/ANightsTaleUI/Controllers/CampaignController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ANightsTaleUI.Controllers
+{
+    public class CampaignController : Controller
+    {
+
+		static string url = "https://localhost:44369/api/Campaign";
+		// GET: Campaign
+		public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Campaign/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Campaign/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Campaign/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
