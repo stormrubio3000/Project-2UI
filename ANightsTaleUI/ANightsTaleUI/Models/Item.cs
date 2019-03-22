665af1f5287e714b950c06a1e946c264132f236e
stormrubio3000 <storm-3000@hotmail.com>
2019-03-22 10:39:39 -0500

    Framework for Items added

---
+++ ANightsTaleUI/ANightsTaleUI/Models/Item.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ANightsTaleUI.Models
+{
+	public class Item
+	{
+
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public string Description { get; set; }
+
+		[Required]
+		public int Type { get; set; }
+
+
+		public int AC { get; set; }
+
+
+		public int? NumDice { get; set; }
+
+
+		public int? NumSides { get; set; }
+
+
+		public int? Mods { get; set; }
+
+		[Required]
+		public string Effects { get; set; }
+	}
+}
