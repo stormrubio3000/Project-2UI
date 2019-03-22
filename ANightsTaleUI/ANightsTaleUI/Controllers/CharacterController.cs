4ca652ad6aa7edc4f09c8dfcc333dc7fd35fa405
stormrubio3000 <storm-3000@hotmail.com>
2019-03-22 15:47:12 -0500

    Framework for the Character Controller

---
+++ ANightsTaleUI/ANightsTaleUI/Controllers/CharacterController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ANightsTaleUI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ANightsTaleUI.Controllers
+{
+    public class CharacterController : Controller
+    {
+
+		static string url = "https://localhost:44369/api/Character";
+		// GET: Character
+		public async Task<ActionResult> Index()
+        {
+			var models = new List<Character>();
+			using (var httpClient = new HttpClient())
+			{
+				var Response = await httpClient.GetAsync(url);
+				if (Response.IsSuccessStatusCode)
+				{
+					var jsonString = await Response.Content.ReadAsStringAsync();
+					List<Character> character = JsonConvert.DeserializeObject<List<Character>>(jsonString);
+					return View(character);
+				}
+			}
+
+			return View(models);
+		}
+
+        // GET: Character/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+			var model = new Character();
+			using (var httpClient = new HttpClient())
+			{
+
+				var Response = await httpClient.GetAsync(url + "/" + id.ToString());
+				if (Response.IsSuccessStatusCode)
+				{
+					var jsonString = await Response.Content.ReadAsStringAsync();
+					Character character = JsonConvert.DeserializeObject<Character>(jsonString);
+					return View(character);
+				}
+			}
+
+			return View(model);
+		}
+
+        // GET: Character/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Character/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Character/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Character/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Character/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Character/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ANightsTaleUI/ANightsTaleUI/Models/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ANightsTaleUI.Models
+{
+	public class Character
+	{
+
+		public int CharacterID { get; set; }
+		public int UserId { get; set; }
+		public string Name { get; set; }
+		public string Bio { get; set; }
+		public int CampaignID { get; set; }
+		public int RaceID { get; set; }
+		public int ClassID { get; set; }
+		public int Experience { get; set; }
+		public int Level { get; set; }
+		public int Str { get; set; }
+		public int Dex { get; set; }
+		public int Con { get; set; }
+		public int Int { get; set; }
+		public int Wis { get; set; }
+		public int Cha { get; set; }
+		public int Speed { get; set; }
+		public int MaxHP { get; set; }
+	}
+}
