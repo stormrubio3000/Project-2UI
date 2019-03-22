726b54fc54294240277b6132ba33457bbfde84c7
stormrubio3000 <storm-3000@hotmail.com>
2019-03-22 10:31:13 -0500

    Framework for the Feats

---
+++ ANightsTaleUI/ANightsTaleUI/Controllers/FeatsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ANightsTaleUI.Controllers
+{
+    public class FeatsController : Controller
+    {
+		static string url = "https://localhost:44369/api/Feats";
+		// GET: Feats
+		public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Feats/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Feats/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Feats/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Feats/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Feats/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Feats/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Feats/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
